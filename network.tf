39f2a79e2b149eed16f8d3f545c310616754e99f
Austin Workman <austin@hashicorp.com>
2019-06-12 16:39:07 -0500

    Adding initial networks for testing.

---
+++ network.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region     = "${var.aws_region}"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "develpoment" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags {
+    Name = "development"
+  }
+}
+
+resource "aws_subnet" "staging" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.2.0/24"
+
+  tags {
+    Name = "staging"
+  }
+}
+
+resource "aws_subnet" "production" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.3.0/24"
+
+  tags {
+    Name = "production"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "aws_region" {
+  description = "region to deploy resources in"
+  default     = "us-east-2"
+}
