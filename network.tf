6855db690fa73cd9512cc51dafacbafa586fae84
Austin Workman <austin@hashicorp.com>
2019-06-12 17:05:41 -0500

    Changing subnet names.

--- network.tf
+++ network.tf
@@ -6,29 +6,29 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "develpoment" {
+resource "aws_subnet" "dev" {
   vpc_id     = "${aws_vpc.main.id}"
   cidr_block = "10.0.1.0/24"
 
   tags {
-    Name = "development"
+    Name = "dev"
   }
 }
 
-resource "aws_subnet" "staging" {
+resource "aws_subnet" "stage" {
   vpc_id     = "${aws_vpc.main.id}"
   cidr_block = "10.0.2.0/24"
 
   tags {
-    Name = "staging"
+    Name = "stage"
   }
 }
 
-resource "aws_subnet" "production" {
+resource "aws_subnet" "prod" {
   vpc_id     = "${aws_vpc.main.id}"
   cidr_block = "10.0.3.0/24"
 
   tags {
-    Name = "production"
+    Name = "prod"
   }
 }
